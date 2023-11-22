a93a4554862d128f20c396f259bbde8a4e163db7
TheDanked <Daniellv_03@outlook.com>
2023-11-22 07:21:28 -0600

    a

--- Parcial 3/Peticion Select/ServerExpress/index.js
+++ Parcial 3/Peticion Select/ServerExpress/index.js
@@ -1,17 +1,36 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mysql = require('mysql2');
 
 app.use(cors());
-app.get('/clientes',(req,res)=>{
-res.json({mensaje:"Server Express respondiendo a get"});
+app.get('/actor',(req,res)=>{
+const conexion = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "0625",
+    database: "sakila", 
+    port: 3306
 });
 
-app.post('/clientes',(req,res)=>{
+conexion.query("SELECT * FROM actor",
+function(err,results,fields){
+    console.log(results)
+    console.log(fields)
+    res.json({results})
+    console.log(err)
+}
+);
+
+});
+
+
+
+app.post('/actor',(req,res)=>{
 res.json({mensaje: "Server express respondiendo a post"})
 });
 
-app.delete('/clientes',(req,res)=>{
+app.delete('/actor',(req,res)=>{
     res.json({mensaje: "Server express respondiendo a delete"})
 });
 app.listen(8082,(req,res)=>{
